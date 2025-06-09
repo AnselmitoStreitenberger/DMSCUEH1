951b0f848097cc2c925be042611e782bf381cccf
AnselmitoStreitenberger <143664422+AnselmitoStreitenberger@users.noreply.github.com>
2025-06-09 16:45:01 -0300

    Add validation, config classes, tests

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from app import create_app
+import os
 
-app = create_app()
+from app import create_app
+from app.config import DevelopmentConfig, ProductionConfig, TestingConfig
+
+config_mapping = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
+
+config_name = os.getenv('FLASK_CONFIG', 'development')
+config_class = config_mapping.get(config_name, DevelopmentConfig)
+
+app = create_app(config_class)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
